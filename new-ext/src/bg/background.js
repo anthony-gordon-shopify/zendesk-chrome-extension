4db8b01342c1b7e41fd3baa6652a2f4e87e8d8ef
Anthony Gordon <anthony.gordon@shopify.com>
2019-05-20 10:15:47 +1200

    enabled cookies

--- new-ext/src/bg/background.js
+++ new-ext/src/bg/background.js
@@ -4,10 +4,17 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
-
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+  chrome.pageAction.show(sender.tab.id);
+  sendResponse();
+});
+
+chrome.cookies.set({
+  url: "https://shopify.zendesk.com/*",
+  name: "CookieVar",
+  value: "123",
+  expirationDate: new Date().getTime() / 1000 + 3600
+});
+
+console.log("background");
